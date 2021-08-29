b7273421d2fee35b9863cb05944c68dd0fbd2643
guno517 <60571418+guno517@users.noreply.github.com>
2021-08-29 14:42:52 +0900

    DOM을 이용한 tag처리와

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const ajax = new XMLHttpRequest();
+
 ajax.open("GET", "https://api.hnpwa.com/v0/news/1.json", false); //true: 비동기적으로 가져온다 false: 동기적으로 수행한다
 ajax.send();
 
-console.log(ajax.response);
+const newsFeed = JSON.parse(ajax.response); // 객체 -> json으로 변환
+
+console.log(newsFeed[0].title);
+
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 10; i++) {
+  const li = document.createElement("li");
+  li.innerHTML = newsFeed[i].title;
+  ul.appendChild(li);
+}
+
+document.getElementById("root").appendChild(ul);
