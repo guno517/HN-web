0cbc2ca1a0ce8ffa17535d9a2a4c67c172618f42
guno517 <60571418+guno517@users.noreply.github.com>
2021-08-29 16:42:54 +0900

    News url 변경

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const ajax = new XMLHttpRequest();
+const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json"; // 해커 뉴스 news 1페이지
 
-ajax.open("GET", "https://api.hnpwa.com/v0/news/1.json", false); //true: 비동기적으로 가져온다 false: 동기적으로 수행한다
+ajax.open("GET", NEWS_URL, false); //true: 비동기적으로 가져온다 false: 동기적으로 수행한다
 ajax.send();
 
 const newsFeed = JSON.parse(ajax.response); // 객체 -> json으로 변환
 
-console.log(newsFeed[0].title);
+console.log(newsFeed);
 
 const ul = document.createElement("ul");
 
