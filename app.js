c3ee7121a50979939ce91261a5e0b7cd60dd31c9
guno517 <60571418+guno517@users.noreply.github.com>
2021-08-30 21:05:34 +0900

    DOM API 줄이기

--- app.js
+++ app.js
@@ -16,32 +16,31 @@ const newsFeed = getData(NEWS_URL);
 const ul = document.createElement("ul");
 
 window.addEventListener("hashchange", function () {
-  // 제목을 클릭 할 때마다 해시 값이 바껴 haschange 함수가 호출된다.
+  // 제목을 클릭 할 때마다 해시 값이 바껴 haschange 함수가 호출된다. -> 내용 화면으로 진입하는 시점(hashchange)
   const id = location.hash.substr(1); //주소와 관련된 정보 제공, substr: () 안의 값 이후부터 끝가지 문자열 출력
 
-  const newsContent = getData(CONTENT_URL);
-  const title = document.createElement("h1");
+  const newsContent = getData(CONTENT_URL.replace("@id", id));
 
-  title.innerHTML = newsContent.title;
+  container.innerHTML = `
+    <h1>${newsContent.title}</h1>
 
-  content.appendChild(title);
+    <div>
+        <A href="#">목록으로</a>
+    </div>
+  `;
 });
 
-for (let i = 0; i < 10; i++) {
-  const div = document.createElement("div");
-  const li = document.createElement("li");
-  const a = document.createElement("a");
+const newsList = [];
 
-  div.innerHTML = `<li><a href = "#${newsFeed[i].id}">
+newsList.push("<ul>");
+
+for (let i = 0; i < 10; i++) {
+  newsList.push(`<li><a href = "#${newsFeed[i].id}">
         ${newsFeed[i].title}  [${newsFeed[i].comments_count}]  
       </a>
-    </li>`;
-
-  a.addEventListener("click", function () {});
-
-  //ul.appendChild(div.children[0]);
-  ul.appendChild(div.firstElementChild);
+    </li>`);
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+newsList.push("</ul>");
+
+container.innerHTML = newsList.join("");
